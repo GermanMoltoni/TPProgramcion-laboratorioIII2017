d557e7dcf4a8f7c0f59c0bdd31412efbcbae68d6
German Moltoni <german.moltoni@gmail.com>
2017-06-27 23:09:12 -0300

    Usuario Api

---
+++ EntidadesApi/UserApi.php
@@ -0,0 +1,80 @@
+<?php
+require_once './Entidades/Usuario.php';
+require_once './Entidades/AuthJWT.php';
+require './vendor/autoload.php';
+ 
+class UserApi extends Usuario{
+   
+    public static function LoginUserApi($request, $response, $args){
+        $data = $request->getParsedBody();
+        $id = filter_var($data['id'], FILTER_SANITIZE_STRING);
+        $password = filter_var($data['password'], FILTER_SANITIZE_STRING);
+        $user = parent::LoginUsuario($id,$password);
+        if($user == false)
+            return $response->withJson(false);
+        return $response->withJson(array('user'=>$user,'token'=>AuthJWT::CrearToken($user)));
+    }
+    public static function LogoutUserApi($request, $response, $args){
+        $data = AuthJWT::GetData($request->getHeader('token')[0]);
+        $id = filter_var($data->id, FILTER_SANITIZE_STRING);
+        return $response->withJson(parent::LogoutUsuario($id));
+    }
+    public static function ListUsersLogApi($request, $response, $args){
+            $userId = filter_var($request->getParams()['id'], FILTER_SANITIZE_STRING);
+            $from = filter_var($request->getParams()['from'], FILTER_SANITIZE_STRING);
+            $to = filter_var($request->getParams()['to'], FILTER_SANITIZE_STRING);
+            return $response->withJson(Usuario::ListarLogsUsuario($userId,$from,$to));
+    }
+    public static function DownUserApi($request, $response, $args){
+        $id = $request->getAttribute('id'); 
+        $id = filter_var($id, FILTER_SANITIZE_STRING);
+        if(Usuario::BorrarUsuario($id))
+            return $response->withJson(Usuario::ListarUsuarios());
+        else
+            return $response->withJson(false);
+    }
+
+    public static function SuspenderUserApi($request, $response, $args) {
+            $id = $request->getAttribute('id'); 
+            $id = filter_var($id, FILTER_SANITIZE_STRING);
+            if(Usuario::ModificarEstadoUsuario($id,0))
+                return $response->withJson(Usuario::ListarUsuarios());
+            else
+                return $response->withJson(false);
+    }
+
+    public static function ListUsersApi($request, $response, $args){
+        return $response->withJson(parent::ListarUsuarios());
+    }
+    
+    public static function HabilitarUserApi($request, $response, $args) {
+            $id = $request->getAttribute('id'); 
+            $id = filter_var($id, FILTER_SANITIZE_STRING);
+            if(Usuario::ModificarEstadoUsuario($id,1))
+                return $response->withJson(Usuario::ListarUsuarios());
+            else
+                return $response->withJson(false);
+    }
+
+    public static function SignUpUserApi($request, $response, $args) {
+        $data = $request->getParsedBody();
+        $user_data=array();
+        $user_data['id'] = filter_var($data['id'], FILTER_SANITIZE_STRING);
+        $user_data['nombre'] = filter_var($data['name'], FILTER_SANITIZE_STRING);
+        $user_data['apellido'] = filter_var($data['surname'], FILTER_SANITIZE_STRING);
+        $user_data['turno'] = filter_var($data['turno'], FILTER_SANITIZE_STRING);
+        $user_data['admin'] = filter_var($data['admin'], FILTER_SANITIZE_STRING);
+        $user_data['estado'] = filter_var($data['state'], FILTER_SANITIZE_STRING);
+        $user_data['password'] = filter_var($data['password'], FILTER_SANITIZE_STRING);    
+        $user = new Usuario($user_data['id'],$user_data['nombre'],$user_data['apellido'],$user_data['password'],$user_data['estado'],$user_data['turno'],$user_data['admin']);
+        if($user->CrearUsuario())
+            return $response->getBody()->write('registrado');
+        else
+            return $response->getBody()->write('ya existe');
+    }
+
+
+}
+
+
+?>
