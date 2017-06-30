57ca229ffedbd69850ad0bfa51d78d0ce6e7c655
German Moltoni <german.moltoni@gmail.com>
2017-06-29 22:49:42 -0300

    cambio de lugar la toma de datos del request

--- EntidadesApi/UserApi.php
+++ EntidadesApi/UserApi.php
@@ -2,27 +2,46 @@
 require_once './Entidades/Usuario.php';
 require_once './Entidades/AuthJWT.php';
 require './vendor/autoload.php';
- 
+
 class UserApi extends Usuario{
    
     public static function LoginUserApi($request, $response, $args){
-        $data = $request->getParsedBody();
-        $id = filter_var($data['id'], FILTER_SANITIZE_STRING);
-        $password = filter_var($data['password'], FILTER_SANITIZE_STRING);
-        $user = parent::LoginUsuario($id,$password);
+        $user_data = $request->getAttribute('user');
+        $user = parent::LoginUsuario($user_data['mail'],$user_data['password']);
         if($user == false)
-            return $response->withJson(false);
+            return $response->withJson(array('error'=>'Datos incorrectos'),201);
         return $response->withJson(array('user'=>$user,'token'=>AuthJWT::CrearToken($user)));
     }
+
+    public static function ListUsersApi($request, $response, $args){
+        return $response->withJson(parent::ListarUsuarios());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static function LogoutUserApi($request, $response, $args){
         $data = AuthJWT::GetData($request->getHeader('token')[0]);
         $id = filter_var($data->id, FILTER_SANITIZE_STRING);
         return $response->withJson(parent::LogoutUsuario($id));
     }
     public static function ListUsersLogApi($request, $response, $args){
-            $userId = filter_var($request->getParams()['id'], FILTER_SANITIZE_STRING);
-            $from = filter_var($request->getParams()['from'], FILTER_SANITIZE_STRING);
-            $to = filter_var($request->getParams()['to'], FILTER_SANITIZE_STRING);
+            $userId = filter_var($request->getParam('id'), FILTER_SANITIZE_STRING);
+            $from = filter_var($request->getParam('from'), FILTER_SANITIZE_STRING);
+            $to = filter_var($request->getParam('to'), FILTER_SANITIZE_STRING);
             return $response->withJson(Usuario::ListarLogsUsuario($userId,$from,$to));
     }
     public static function DownUserApi($request, $response, $args){
@@ -43,9 +62,7 @@ class UserApi extends Usuario{
                 return $response->withJson(false);
     }
 
-    public static function ListUsersApi($request, $response, $args){
-        return $response->withJson(parent::ListarUsuarios());
-    }
+    
     
     public static function HabilitarUserApi($request, $response, $args) {
             $id = $request->getAttribute('id'); 
@@ -58,11 +75,10 @@ class UserApi extends Usuario{
 
     public static function AltaUsuarioApi($request, $response, $args) {
         $user_data = $request->getAttribute('user');
-        $user = new Usuario($user_data['mail'],$user_data['nombre'],$user_data['apellido'],$user_data['password'],$user_data['estado'],$user_data['turno'],$user_data['admin']);
-        if($user->CrearUsuario())
-            return $response->getBody()->write('registrado');
-        else
-            return $response->getBody()->write('ya existe');
+        $path = $request->getAttribute('pathFoto');
+        $user = new Usuario($user_data['mail'],$user_data['nombre'],$user_data['apellido'],$user_data['password'],$user_data['estado'],$user_data['turno'],$user_data['admin'],$path);
+        if(!$user->CrearUsuario())
+            return $response->withJson(array('error'=>'No se pudo crear el usuario'));
     }
 
 
