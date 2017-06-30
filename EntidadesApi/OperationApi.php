67fa4ab9bb851ebb00f8e30001095f4b1a729cda
German Moltoni <german.moltoni@gmail.com>
2017-06-29 23:15:19 -0300

    cambio getparam

--- EntidadesApi/OperationApi.php
+++ EntidadesApi/OperationApi.php
@@ -3,18 +3,24 @@ require_once './Entidades/Operaciones.php';
 require './vendor/autoload.php';
 class OperationApi extends Operacion{
     public static function ListOperationApi($request, $response, $args) {
-    $userId = filter_var($request->getParams()['id'], FILTER_SANITIZE_STRING);
-    $from = filter_var($request->getParams()['from'], FILTER_SANITIZE_STRING);
-    $to =filter_var( $request->getParams()['to'], FILTER_SANITIZE_STRING);
-    return $response->withJson(parent::ListarOperaciones($userId,$from,$to));
+    $userId = filter_var($request->getParam('id'), FILTER_SANITIZE_STRING);
+    $from = filter_var($request->getParam('from'), FILTER_SANITIZE_STRING);
+    $to = filter_var($request->getParam('to'), FILTER_SANITIZE_STRING);
+    $operaciones = parent::ListarOperaciones($userId,$from,$to);
+    if(count($operaciones) == 0)
+        return $response->withJson(array('error'=>'No hay operaciones Cargadas'));
+    return $response->withJson($operaciones);
   
 }
-public static function QOperationUserApi($request, $response, $args) {
+public static function ListOperationUserApi($request, $response, $args) {
 
-    $userId = filter_var($request->getParams()['id'], FILTER_SANITIZE_STRING);
-    $from = filter_var($request->getParams()['from'], FILTER_SANITIZE_STRING);
-    $to =filter_var( $request->getParams()['to'], FILTER_SANITIZE_STRING);
-    return $response->withJson(parent::CantidadOperacionesPorUsuario($userId,$from,$to));
+    $userId = filter_var($request->getParam('id'), FILTER_SANITIZE_STRING);
+    $from = filter_var($request->getParam('from'), FILTER_SANITIZE_STRING);
+    $to = filter_var($request->getParam('to'), FILTER_SANITIZE_STRING);
+    $operaciones = parent::CantidadOperacionesPorUsuario($userId,$from,$to);
+    if($operaciones[0]->idUser == null)
+        return $response->withJson(array('error'=>'No hay operaciones Cargadas'));
+    return $response->withJson($operaciones);
   
 }
 
