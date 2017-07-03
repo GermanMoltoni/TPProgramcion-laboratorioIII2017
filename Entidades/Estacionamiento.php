ec4e8061f71248f536e2802dc393a985d4441c43
German Moltoni <german.moltoni@gmail.com>
2017-07-03 15:27:00 -0300

    lugar mas,menos y nunca utilizado

--- Entidades/Estacionamiento.php
+++ Entidades/Estacionamiento.php
@@ -58,22 +58,73 @@ class Estacionamiento{
         else
             return array('error'=>'El vehiculo no se encuentra estacionado');
     }
+    /*
+    *   Retorna el lugar mas utilizado junto a la cantidad de veces.
+    *   from: Fecha desde
+    *   to: Fecha Hasta
+    *
+    */
+    static function LugarMasUtilizado($from=null,$to=null){
+        $objDB = AccesoDatos::DameUnObjetoAcceso();
+        if($from != null && $to!=null)
+        {
+            if(self::checkDateOrDateTime($from) && self::checkDateOrDateTime($to))
+                $consulta = $objDB->RetornarConsulta("SELECT s.idCochera as cochera,s.veces as veces FROM (SELECT idCochera,count(*) as veces FROM operaciones  WHERE cast(entrada  as date) BETWEEN '".$from."' AND '".$to."' GROUP BY idCochera) as s where s.veces IN (SELECT MAX(o.veces) FROM(SELECT idCochera,count(*) as veces FROM operaciones  WHERE cast(entrada  as date) BETWEEN '".$from."' AND '".$to."' GROUP BY idCochera) as o)");
+            else
+                $consulta = $objDB->RetornarConsulta("SELECT s.idCochera as cochera,s.veces as veces FROM (SELECT idCochera,count(*) as veces FROM operaciones  WHERE cast(entrada  as datetime) BETWEEN '".$from."' AND '".$to."' GROUP BY idCochera) as s where s.veces IN (SELECT MAX(o.veces) FROM(SELECT idCochera,count(*) as veces FROM operaciones  WHERE cast(entrada  as datetime) BETWEEN '".$from."' AND '".$to."' GROUP BY idCochera) as o)");
 
-    static function LugarMasUtilizado(){
+        }
+        elseif ($from != null) 
+            $consulta = $objDB->RetornarConsulta("SELECT s.idCochera as cochera,s.veces as veces FROM (SELECT idCochera,count(*) as veces FROM operaciones  WHERE entrada LIKE '%".$from."%'  GROUP BY idCochera) as s where s.veces IN (SELECT MAX(o.veces) FROM(SELECT idCochera,count(*) as veces FROM operaciones  WHERE entrada LIKE '%".$from."%'  GROUP BY idCochera) as o)");
+        else
+            $consulta = $objDB->RetornarConsulta("SELECT s.idCochera as cochera,s.veces as veces FROM (SELECT idCochera,count(*) as veces FROM operaciones GROUP BY idCochera) as s where s.veces IN (SELECT MAX(o.veces) FROM(SELECT idCochera,count(*) as veces FROM operaciones GROUP BY idCochera) as o)");
+        $consulta->execute();
+        return $consulta->fetchAll(PDO::FETCH_OBJ);
+        
+    }
+    /*
+    *   Retorna el lugar menos utilizado junto a la cantidad de veces.
+    *   from: Fecha desde
+    *   to: Fecha Hasta
+    *
+    */
+    static function LugarMenosUtilizado($from=null,$to=null){
         $objDB = AccesoDatos::DameUnObjetoAcceso();
-        $consulta = $objDB->RetornarConsulta("SELECT s.idCochera as cochera,s.veces as veces FROM (SELECT idCochera,count(*) as veces FROM operaciones GROUP BY idCochera) as s where s.veces IN (SELECT MAX(o.veces) FROM(SELECT idCochera,count(*) as veces FROM operaciones GROUP BY idCochera) as o)");
+        if($from != null && $to!=null)
+        {
+            if(self::checkDateOrDateTime($from) && self::checkDateOrDateTime($to))
+                $consulta = $objDB->RetornarConsulta("SELECT s.idCochera as cochera,s.veces as veces FROM (SELECT idCochera,count(*) as veces FROM operaciones WHERE cast(entrada  as date) BETWEEN '".$from."' AND '".$to."'  GROUP BY idCochera) as s where s.veces IN (SELECT MIN(o.veces) FROM(SELECT idCochera,count(*) as veces FROM operaciones WHERE cast(entrada  as date) BETWEEN '".$from."' AND '".$to."' GROUP BY idCochera) as o)");
+            else
+                $consulta = $objDB->RetornarConsulta("SELECT s.idCochera as cochera,s.veces as veces FROM (SELECT idCochera,count(*) as veces FROM operaciones WHERE cast(entrada  as datetime) BETWEEN '".$from."' AND '".$to."'  GROUP BY idCochera) as s where s.veces IN (SELECT MIN(o.veces) FROM(SELECT idCochera,count(*) as veces FROM operaciones WHERE cast(entrada  as datetime) BETWEEN '".$from."' AND '".$to."' GROUP BY idCochera) as o)");
+
+        }   
+         elseif ($from != null) 
+            $consulta = $objDB->RetornarConsulta("SELECT s.idCochera as cochera,s.veces as veces FROM (SELECT idCochera,count(*) as veces FROM operaciones WHERE entrada LIKE '%".$from."%'   GROUP BY idCochera) as s where s.veces IN (SELECT MIN(o.veces) FROM(SELECT idCochera,count(*) as veces FROM operaciones WHERE entrada LIKE '%".$from."%'  GROUP BY idCochera) as o)");
+        else
+            $consulta = $objDB->RetornarConsulta("SELECT s.idCochera as cochera,s.veces as veces FROM (SELECT idCochera,count(*) as veces FROM operaciones GROUP BY idCochera) as s where s.veces IN (SELECT MIN(o.veces) FROM(SELECT idCochera,count(*) as veces FROM operaciones GROUP BY idCochera) as o)");
         $consulta->execute();
         return $consulta->fetchAll(PDO::FETCH_OBJ);
     }
-    static function LugarMenosUtilizado(){
+    /*
+    *   Retorna el lugar nunca  utilizado junto a la cantidad de veces.
+    *   from: Fecha desde
+    *   to: Fecha Hasta
+    *
+    */
+    static function LugarNuncaUtilizado($from=null,$to=null){
         $objDB = AccesoDatos::DameUnObjetoAcceso();
-        $consulta = $objDB->RetornarConsulta("SELECT s.idCochera as cochera,s.veces as veces FROM (SELECT idCochera,count(*) as veces FROM operaciones GROUP BY idCochera) as s where s.veces IN (SELECT MIN(o.veces) FROM(SELECT idCochera,count(*) as veces FROM operaciones GROUP BY idCochera) as o)");
-        $consulta->execute();
-        return $consulta->fetchAll(PDO::FETCH_OBJ);
-    }
-    static function LugarNuncaUtilizado(){
-        $objDB = AccesoDatos::DameUnObjetoAcceso();
-        $consulta = $objDB->RetornarConsulta("SELECT idCochera FROM operaciones GROUP BY idCochera");
+        if($from != null && $to!=null)
+        {         
+            if(self::checkDateOrDateTime($from) && self::checkDateOrDateTime($to))   
+                $consulta = $objDB->RetornarConsulta("SELECT idCochera FROM operaciones WHERE cast(entrada  as date) BETWEEN '".$from."' AND '".$to."' GROUP BY idCochera");
+            else
+                $consulta = $objDB->RetornarConsulta("SELECT idCochera FROM operaciones WHERE cast(entrada  as datetime) BETWEEN '".$from."' AND '".$to."' GROUP BY idCochera");
+
+        }
+        elseif ($from != null) 
+            $consulta = $objDB->RetornarConsulta("SELECT idCochera FROM operaciones WHERE entrada LIKE '%".$from."%'  GROUP BY idCochera");
+        else
+            $consulta = $objDB->RetornarConsulta("SELECT idCochera FROM operaciones GROUP BY idCochera");
         $consulta->execute();
         $usados= $consulta->fetchAll(PDO::FETCH_COLUMN,0);
         $pisos = Piso::BuscarPiso();
@@ -86,7 +137,13 @@ class Estacionamiento{
         sort($lugares,SORT_NUMERIC);
         return $lugares;
     }
+    private static function checkDateOrDateTime($var){
+    if(preg_match('/\d{4}-\d{2}-\d{2}$/',$var))
+        return 1;   
+    elseif(preg_match('/\d{4}-\d{2}-\d{2}\h\d{2}:\d{2}:\d{2}$/',$var))
+        return 0;   
     
+}
 
 }
 
