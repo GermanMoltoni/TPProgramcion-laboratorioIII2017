d943d9bdef1656cdf9df903977907551bbc2445f
German Moltoni <german.moltoni@gmail.com>
2017-07-03 15:26:13 -0300

    ordeno rutas

--- index.php
+++ index.php
@@ -22,64 +22,78 @@ $app->add(function ($req, $res, $next) {
 
 
 $app->group('/usuario', function () {
-    $this->get('/listar',\UserApi::class . ':ListUsersApi');
+    $this->get('/listar',\UserApi::class . ':ListaUserApi');
  
-    $this->get('/listarLogs', \UserApi::class .':ListUsersLogApi');
+    $this->get('/listarLogs', \UserApi::class .':ListaLogUserApi')
+                ->add(\AuthUser::class.':verificarFormTiempo');
 
-    $this->delete('/baja', \UserApi::class .':DownUserApi')->add(\AuthUser::class.':verificarUsuario');
+    $this->delete('/baja', \UserApi::class .':BajaUserApi')
+                ->add(\AuthUser::class.':verificarUsuario');
 
-    $this->put('/suspender', \UserApi::class .':SuspenderUserApi')->add(\AuthUser::class.':verificarUsuario');
+    $this->put('/suspender', \UserApi::class .':SuspenderUserApi')
+                ->add(\AuthUser::class.':verificarUsuario');
 
-    $this->put('/habilitar', \UserApi::class .':HabilitarUserApi')->add(\AuthUser::class.':verificarUsuario');
+    $this->put('/habilitar', \UserApi::class .':HabilitarUserApi')
+                ->add(\AuthUser::class.':verificarUsuario');
 
     $this->post('/alta', \UserApi::class .':AltaUsuarioApi')
                 ->add(\Imagen::class.':SubirImagenUsuario')
                 ->add(\AuthUser::class.':VerificarArchivo')
                 ->add(\AuthUser::class.':VerificarUsuarioDup')
                 ->add(\AuthUser::class.':VerificarCamposFormUser');
-
-
-
+    $this->post('/modificar', \UserApi::class .':ModificarUsuarioApi')
+                ->add(\Imagen::class.':ModificarImagenUsuario')
+                ->add(\AuthUser::class.':VerificarArchivo')
+                ->add(\AuthUser::class.':verificarModificacionUsuario')
+                ->add(\AuthUser::class.':VerificarUsuario')
+                ->add(\AuthUser::class.':VerificarModifFormUser');
 
 })->add(\AuthUser::class.':admin');
 
 
 
-$app->post('/login',\UserApi::class . ':LoginUserApi')->add(\AuthUser::class.':VerificarFormLogin')->add(\AuthUser::class.':login');
-$app->get('/logout',\UserApi::class  .':LogoutUserApi')->add(\AuthUser::class.':users');
+$app->post('/login',\UserApi::class . ':LoginUserApi')
+                ->add(\AuthUser::class.':VerificarFormLogin')
+                ->add(\AuthUser::class.':login');
+$app->get('/logout',\UserApi::class  .':LogoutUserApi')
+                ->add(\AuthUser::class.':users');
 
 
 $app->group('/estacionamiento', function () {
-    $this->post('/ingreso',\ParkingApi::class . ':ParkCarApi')->add(\AuthUser::class.':VerificarFormIngreso');
+    $this->post('/ingreso',\ParkingApi::class . ':ParkCarApi')
+                    ->add(\AuthUser::class.':VerificarFormIngreso');
     $this->delete('/egreso/{patente}',\ParkingApi::class . ':RemoveCarApi');
-
     $this->get('/listaCocheras', function (Request $request, Response $response) {
         return $response->withJson(array('pisos'=>Piso::BuscarPiso(),'ocupados'=>Operacion::ListarOperacionesActivas()));
-
-
-
-
 });
+        $this->group('/lugares', function () {
+            $this->get('/menosUtilizado',\ParkingApi::class.':LugarMenosUtilizadoApi');
+
+$this->get('/nuncaUtilizado',\ParkingApi::class.':LugarNuncaUtilizadoApi');
+
+
+$this->get('/masUtilizado',\ParkingApi::class.':LugarMasUtilizadoApi');
+                
+
+})->add(\AuthUser::class.':verificarFormTiempo');
+
 
 
 
 })->add(\AuthUser::class.':users');
 
 $app->group('/operaciones', function () {
-    $this->get('/listar',\OperationApi::class . ':ListOperationApi');
-    $this->get('/cantidadoperacionesUsuarios',\OperationApi::class . ':ListOperationUserApi');
+    $this->get('/listar',\OperationApi::class . ':ListOperationApi')
+                ->add(\AuthUser::class.':verificarFormTiempo');
+    $this->get('/operacionesUsuarios',\OperationApi::class . ':ListOperationUserApi')
+                ->add(\AuthUser::class.':verificarFormTiempo');
 
 })->add(\AuthUser::class.':admin');
 
-$app->get('/estacionamiento/listaEstadisticaCocheras', function (Request $request, Response $response) {
-    return $response->withJson(Piso::EstadisticaCocheras('nunca'));
-});
-
-
-$app->get('/a',\ParkingApi::class.':LugarNuncaUtilizadoApi');
 
 
 
+$app->get('/a', \UserApi::class . ':BuscarUsuarioPorMail');
 
 
 
