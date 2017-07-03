51f306b3cd875457ef26f5987fea3a11bf19bfb4
German Moltoni <german.moltoni@gmail.com>
2017-07-03 15:25:02 -0300

    lugar mas,menos nunca utilizado

--- EntidadesApi/ParkingApi.php
+++ EntidadesApi/ParkingApi.php
@@ -7,24 +7,28 @@ require './vendor/autoload.php';
 class ParkingApi extends Estacionamiento{
     public static function ParkCarApi($request, $response, $args) {
         $vehiculo = $request->getAttribute('vehiculo');
+        $idUser = $request->getAttribute('idUser');
         $auto = new Auto($vehiculo['patente'],$vehiculo['marca'],$vehiculo['color'],$vehiculo['especial']);
-        return $response->withJson(parent::Estacionar($auto,1));
+        return $response->withJson(parent::Estacionar($auto,$idUser));
     }
     public static function RemoveCarApi($request, $response, $args) {
         $patente = $request->getAttribute('patente'); 
         return $response->withJson(parent::RetirarAuto(filter_var($patente, FILTER_SANITIZE_STRING)));
     }
     public static function LugarMasUtilizadoApi($request, $response, $args) {
-
-        return $response->withJson(parent::LugarMasUtilizado());
+        $from=$request->getParam('from');
+        $to=$request->getParam('to');
+        return $response->withJson(parent::LugarMasUtilizado($from,$to));
     }
     public static function LugarMenosUtilizadoApi($request, $response, $args) {
-
-        return $response->withJson(parent::LugarMenosUtilizado());
+$from=$request->getParam('from');
+        $to=$request->getParam('to');
+        return $response->withJson(parent::LugarMenosUtilizado($from,$to));
     }
     public static function LugarNuncaUtilizadoApi($request, $response, $args) {
-
-        return $response->withJson(parent::LugarNuncaUtilizado());
+$from=$request->getParam('from');
+        $to=$request->getParam('to');
+        return $response->withJson(parent::LugarNuncaUtilizado($from,$to));
     }
 }
 
