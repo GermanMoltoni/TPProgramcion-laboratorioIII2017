91657530e12e57cdbacaf41f4093d768854bebe5
German Moltoni <german.moltoni@gmail.com>
2017-08-01 02:03:11 -0300

    sin datos

--- estacionamiento.sql
+++ estacionamiento.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 27-07-2017 a las 05:56:58
+-- Tiempo de generación: 01-08-2017 a las 07:00:08
 -- Versión del servidor: 10.1.24-MariaDB
 -- Versión de PHP: 7.1.6
 
@@ -35,15 +35,6 @@ CREATE TABLE `autos` (
   `especial` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
---
--- Volcado de datos para la tabla `autos`
---
-
-INSERT INTO `autos` (`patente`, `color`, `marca`, `especial`) VALUES
-('abc123', 'Azul', 'Peugeot', 0),
-('abc1234', 'Azul', 'Peugeot', 0),
-('abc12345', 'Azul', 'Peugeot', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -56,44 +47,6 @@ CREATE TABLE `logusuarios` (
   `entrada` datetime NOT NULL,
   `token` varchar(528) COLLATE utf8_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `logusuarios`
---
-
-INSERT INTO `logusuarios` (`id`, `idUsuario`, `entrada`, `token`) VALUES
-(1, 1, '2017-07-01 13:55:00', '2017-07-02 14:06:00'),
-(2, 3, '2017-07-02 14:00:00', '2017-07-03 14:01:00'),
-(3, 1, '2017-07-03 14:01:00', '2017-07-03 14:06:00'),
-(4, 1, '2017-07-26 19:42:00', ''),
-(5, 1, '2017-07-26 23:51:00', ''),
-(6, 1, '2017-07-26 23:52:00', ''),
-(7, 1, '2017-07-26 23:53:00', ''),
-(8, 1, '2017-07-26 23:58:00', ''),
-(9, 1, '2017-07-26 23:58:00', ''),
-(10, 1, '2017-07-26 23:59:00', ''),
-(11, 1, '2017-07-27 00:00:00', ''),
-(12, 1, '2017-07-27 00:00:00', ''),
-(13, 1, '2017-07-27 00:00:00', ''),
-(14, 1, '2017-07-27 00:00:00', ''),
-(15, 1, '2017-07-27 00:02:00', ''),
-(16, 1, '2017-07-27 00:02:00', ''),
-(17, 1, '2017-07-27 00:03:00', ''),
-(18, 1, '2017-07-27 00:03:00', ''),
-(19, 1, '2017-07-27 00:04:00', ''),
-(20, 1, '2017-07-27 00:04:00', ''),
-(21, 1, '2017-07-27 00:04:00', ''),
-(22, 1, '2017-07-27 00:04:00', ''),
-(23, 1, '2017-07-27 00:05:00', ''),
-(24, 1, '2017-07-27 00:06:00', ''),
-(25, 1, '2017-07-27 00:06:00', ''),
-(26, 1, '2017-07-27 00:07:00', ''),
-(27, 1, '2017-07-27 00:08:00', ''),
-(28, 1, '2017-07-27 00:08:00', ''),
-(29, 1, '2017-07-27 00:08:00', ''),
-(30, 1, '2017-07-27 00:09:00', ''),
-(31, 1, '2017-07-27 00:13:00', ''),
-(32, 1, '2017-07-27 00:32:00', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDExMjYzNDksImV4cCI6MTUwMTE2MjM0OSwiZGF0YSI6eyJpZCI6MSwibWFpbCI6ImFkbWluQGFkbWluIiwibm9tYnJlIjoiR2VybWFuIiwiYXBlbGxpZG8iOiJBZG1pbiIsInBhc3N3b3JkIjoiMTIzIiwiZXN0YWRvIjoxLCJ0dXJubyI6bnVsbCwiYWRtaW4iOjEsImVudHJhZGEiOm51bGwsInBhdGhGb3RvIjpudWxsLCJzYWxpZGEiOm51bGx9LCJhdWQiOiI0YjQ1NWM5YzU5MzQxYjZjNmYxMzI2NDIwNzE3M2QzODI3OWJhMzg1IiwiYXBwIjoiQXBpIFJlc3QgSldUIn0.ojeE3-2m-FAKHEI5N_zz9rtEmOsPBFctJoEXOto79lE');
 
 -- --------------------------------------------------------
 
@@ -110,15 +63,6 @@ CREATE TABLE `operaciones` (
   `salida` datetime DEFAULT NULL,
   `pago` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `operaciones`
---
-
-INSERT INTO `operaciones` (`id`, `idUser`, `patente`, `idCochera`, `entrada`, `salida`, `pago`) VALUES
-(1, 2, 'abc123', 106, '2017-07-03 13:28:00', '2017-07-03 14:30:00', 10),
-(2, 1, 'abc1234', 106, '2017-07-03 14:32:00', '2017-07-03 15:36:00', 10),
-(3, 1, 'abc12345', 102, '2017-07-02 19:54:00', '2017-07-03 15:40:00', 90);
 
 -- --------------------------------------------------------
 
@@ -184,8 +128,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `mail`, `password`, `estado`, `admin`, `turno`, `pathFoto`) VALUES
-(1, 'German', 'Admin', 'admin@admin', '123', 1, 1, NULL, NULL),
-(3, 'Juan', 'Lopez', 'juan@tarde', '123', 1, 0, 2, 'juan@tarde.png');
+(1, 'German', 'Admin', 'admin@admin', '123', 0, 1, NULL, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -235,12 +178,12 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `logusuarios`
 --
 ALTER TABLE `logusuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `operaciones`
 --
 ALTER TABLE `operaciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `pisos`
 --
@@ -250,7 +193,7 @@ ALTER TABLE `pisos`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
