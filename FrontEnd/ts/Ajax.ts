64fa1617cf7c7a5a9d88d07dd9e2b0eebc78a4f3
German Moltoni <german.moltoni@gmail.com>
2017-11-28 21:43:02 -0300

    cambios

--- FrontEnd/ts/Ajax.ts
+++ FrontEnd/ts/Ajax.ts
@@ -1,18 +1,17 @@
-
-declare var $;
-class Ajax{
+import * as $ from "jquery";
+export class Ajax{
     private static url:string = 'http://germanmoltoni.esy.es/';
-    public static get(done, fail, data = null) {
+    public static get(path:string,done:any, fail:any, data?:any) {
         $.ajax({
-            url: Ajax.url,
+            url: Ajax.url+path,
             type: 'GET',
             dataType: 'json',
             data: data
         }).done(done).fail(fail);
     }
-    public static post(data, done, fail) {
+    public static post(path:string,data:any, done:any, fail:any) {
         $.ajax({
-            url: Ajax.url,
+            url: Ajax.url+path,
             type: 'POST',
             dataType: 'json',
             data: data
--- FrontEnd/ts/Usuario.ts
+++ FrontEnd/ts/Usuario.ts
@@ -1,17 +1,18 @@
-declare var $;
-class Usuario{
-    private id:number;
+import * as $ from "jquery";
+import {Ajax} from './Ajax';
+export class Usuario{
+    private id:number| undefined;
     private mail:string;
     private nombre:string;
     private apellido:string;
     private password:string;
-    private turno;
-    private admin:boolean;
-    private entrada:string;
-    private pathFoto:string;
-    private estado:boolean;
-    private token:string;
-    constructor(mail:string,nombre:string,apellido:string,password:string,estado:boolean,admin:boolean,turno?:number,pathFoto?:string,id?:number,entrada?:string,token?:string){
+    private turno:number | undefined;
+    private admin:string;
+    private entrada:string | undefined;
+    private pathFoto:string | undefined;
+    private estado:string;
+    private token:string | undefined;
+    constructor(mail:string,nombre:string,apellido:string,password:string,estado:string,admin:string,turno?:number,pathFoto?:string,id?:number,entrada?:string,token?:string){
         this.id=id;
         this.mail=mail;
         this.nombre=nombre;
@@ -25,10 +26,10 @@ class Usuario{
         this.token=token;
     }
     public static crear(){
-        let usuario = new Usuario();
+        //let usuario = new Usuario();
     }
     public static listar(){
-        
+        Ajax.post('login',{usuario:'germanAdmin',password:'123'},(e:any)=>{console.log(e)},()=>{});
     }
     private static getForm(){
         let nombre = $("#nombre").val();
@@ -45,11 +46,17 @@ class Usuario{
         $("#nombre").val(this.nombre);
         $("#apellido").val(this.apellido);
         $("#password").val('');
-        $("#id").val(this.id);
+        $("#id").val(this.id != undefined?this.id:'');
         $("#mail").val(this.mail);
-        $("#turno").val(this.turno);
-        $("#admin").val(this.admin);
-        $("#estado").val(this.estado);
+        $("#turno").val(this.turno != undefined?this.turno:'');
+        $("#admin").val(this.admin != undefined?this.admin:'');
+        $("#estado").val(this.estado != undefined?this.estado:'');
         $("#pathFoto").val('');
     }
 }
+
+
+$(document).ready(()=>{
+    Usuario.listar();
+}
+);
